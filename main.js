04c4047696c016e4fd3fd289d40b8d5dcc0acf05
Ornsteinerr <nobelsiu1@gmail.com>
2022-03-10 19:13:47 -0800

    Add further css styling and adjustable square amount

--- main.js
+++ main.js
@@ -1,31 +1,52 @@
-// Initialize 16x16 grid of square divs
+/* Variables */
+let numDivs;
+let isGridActive = false;
+const gridWidth = 1000;
 
-initiateDivs(16);
+/* Event listeners */
+
+// Submit number of squares button listener
+const numSquareSet = document.querySelector('#numSquareSet');
+numSquareSet.addEventListener('click', initiateDivs);
+
+
+/* Functions */
 
 function initiateDivs(numDivs){
+
+    if (isGridActive === true){
+        clearSquares();
+    }
+
+    numDivs = document.getElementById('numSquareInput').value;
+    console.log(`numDivs: ${numDivs}`);
     const grid = document.querySelector('.grid');
 
     // Set grid size
-    const gridWidth = 1000;
     grid.style.width = gridWidth + 'px';;
     grid.style.height = gridWidth + 'px';;
 
     // Generate squares
-    for (let i = 0; i < numDivs; i++){
+    for (let i = 0; i < Math.pow(numDivs, 2); i++){
         const squareDiv = document.createElement('div');
         // Add styling to square
-        squareDiv.style.minWidth = (gridWidth / 4 - 10) + "px";
-        console.log(gridWidth / 4 / 10);
-        squareDiv.style.minHeight = (gridWidth / 4 - 10) + "px";
+        squareDiv.style.minWidth = (gridWidth / numDivs) + "px";
+        console.log(gridWidth / numDivs);
+        squareDiv.style.minHeight = (gridWidth / numDivs) + "px";
         squareDiv.classList.add('square');
         // Add square
         grid.appendChild(squareDiv);
     }
 
+    // Add listeners for mouseover to change color
     const squareDivAll = document.querySelectorAll('.square');
     squareDivAll.forEach((square) => {
         square.addEventListener('mouseover', changeColor);
         })
+
+    // Set grid active to true
+    isGridActive = true;
+
 }
 
 function changeColor(){
@@ -34,3 +55,31 @@ function changeColor(){
     // Remove highlight after 500 ms
     setTimeout(() => this.style.backgroundColor = "", 500);
 }
+
+function clearSquares(){
+    console.log('triggered');
+    const squareDivAll = document.querySelectorAll('.square');
+    squareDivAll.forEach((square)=>{
+        square.remove();
+    })
+}
+
+/* Slider dynamic display */
+
+// const numSquareInput = document.getElementById('#numSquareInput');
+// console.log(numSquareInput.value);
+// const numSquares = document.getElementById('#numSquares');
+// numSquares.innerHtml = numSquareInput.value;
+
+// numSquareInput.oninput = function (){
+//     numSquares.innerHtml = this.value;
+// }
+
+const numSquareInput = document.getElementById('numSquareInput');
+numDivs = document.getElementById('numSquareInput').value;
+const numSquares = document.getElementById('numSquares');
+console.log (numDivs);
+numSquareInput.addEventListener("input", function(){
+    numDivs = document.getElementById('numSquareInput').value;
+    numSquares.textContent = numDivs;
+})
