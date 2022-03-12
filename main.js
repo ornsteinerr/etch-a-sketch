d21be7f85812b1cc98c66285ab31cf956da49c95
Ornsteinerr <nobelsiu1@gmail.com>
2022-03-12 09:33:20 -0800

    Add color and erase modes

--- main.js
+++ main.js
@@ -1,14 +1,47 @@
 /* Variables */
 let numDivs;
 let isGridActive = false;
-const gridWidth = 1000;
+let mode = "color";
+const GRID_WIDTH = 1000;
+const DEFAULT_COLOR = "rgb(240, 235, 235)";
 
 /* Event listeners */
 
 // Submit number of squares button listener
-const numSquareSet = document.querySelector('#numSquareSet');
-numSquareSet.addEventListener('click', initiateDivs);
+const numSquareSetButton = document.querySelector('#numSquareSetButton');
+numSquareSetButton.addEventListener('click', initiateDivs);
 
+// Slider listener
+const numSquareInput = document.getElementById('numSquareInput');
+numDivs = document.getElementById('numSquareInput').value;
+const numSquares = document.getElementById('numSquares');
+numSquareInput.addEventListener("input", function(){
+    // Update slider value
+    numDivs = document.getElementById('numSquareInput').value;
+    numSquares.textContent = `${numDivs} x ${numDivs}`;
+})
+
+// Listener for mouseholds
+const body = document.querySelector('body');
+let mouseDown = false;
+body.addEventListener('mousedown', function(){
+    mouseDown = true;
+ })
+body.addEventListener('mouseup', function(){
+     mouseDown = false;
+ })
+
+// Clear Grid listener
+const clearButton = document.querySelector("#clearButton");
+clearButton.addEventListener("click", clearColors);
+
+// Erase listener
+const eraseButton = document.querySelector("#eraseButton");
+eraseButton.addEventListener("click", setMode);
+
+// Color listener
+const colorButton = document.querySelector("#colorButton");
+colorButton.addEventListener("click", setMode);
 
 /* Functions */
 
@@ -19,67 +52,84 @@ function initiateDivs(numDivs){
     }
 
     numDivs = document.getElementById('numSquareInput').value;
-    console.log(`numDivs: ${numDivs}`);
     const grid = document.querySelector('.grid');
 
     // Set grid size
-    grid.style.width = gridWidth + 'px';;
-    grid.style.height = gridWidth + 'px';;
+    grid.style.width = GRID_WIDTH + 'px';;
+    grid.style.height = GRID_WIDTH + 'px';;
 
     // Generate squares
     for (let i = 0; i < Math.pow(numDivs, 2); i++){
         const squareDiv = document.createElement('div');
         // Add styling to square
-        squareDiv.style.minWidth = (gridWidth / numDivs) + "px";
-        console.log(gridWidth / numDivs);
-        squareDiv.style.minHeight = (gridWidth / numDivs) + "px";
+        squareDiv.style.minWidth = (GRID_WIDTH / numDivs) + "px";
+        squareDiv.style.minHeight = (GRID_WIDTH / numDivs) + "px";
         squareDiv.classList.add('square');
         // Add square
         grid.appendChild(squareDiv);
     }
 
-    // Add listeners for mouseover to change color
+    // Add listeners for mouseover to manipulate colors
     const squareDivAll = document.querySelectorAll('.square');
     squareDivAll.forEach((square) => {
         square.addEventListener('mouseover', changeColor);
+        square.addEventListener('click', changeColor);
         })
-
+    
+    
     // Set grid active to true
     isGridActive = true;
 
+    // Add clear button
+    const tools = document.querySelector("#tools");
+    tools.classList.remove("hidden");
+
 }
 
-function changeColor(){
-    // Highlight mouseover square
-    this.style.backgroundColor = 'yellow';
-    // Remove highlight after 500 ms
-    setTimeout(() => this.style.backgroundColor = "", 500);
+function setMode(){
+    const allButtons = document.querySelectorAll('button');
+    allButtons.forEach((button) => {
+        button.classList.remove('active');
+    })
+    this.classList.toggle('active');
+    mode = this.name;
 }
+
+function changeColor(e){
+    // Prevent coloring when mouseover and mouse is not held down
+    if (e.type === 'mouseover' && !mouseDown){
+        return;
+    }
+    switch (mode){
+        case "color":
+            this.style.backgroundColor = '#' + getRandomColor();
+            return;
+        case "erase":
+            this.style.backgroundColor = "";
+            return;
+    }
+}
+
 
 function clearSquares(){
-    console.log('triggered');
     const squareDivAll = document.querySelectorAll('.square');
     squareDivAll.forEach((square)=>{
         square.remove();
     })
 }
 
-/* Slider dynamic display */
+function clearColors(){
+    const squareDivAll = document.querySelectorAll('.square');
+    squareDivAll.forEach((square)=>{
+        square.style.backgroundColor = '';
+    })
+}
 
-// const numSquareInput = document.getElementById('#numSquareInput');
-// console.log(numSquareInput.value);
-// const numSquares = document.getElementById('#numSquares');
-// numSquares.innerHtml = numSquareInput.value;
 
-// numSquareInput.oninput = function (){
-//     numSquares.innerHtml = this.value;
-// }
 
-const numSquareInput = document.getElementById('numSquareInput');
-numDivs = document.getElementById('numSquareInput').value;
-const numSquares = document.getElementById('numSquares');
-console.log (numDivs);
-numSquareInput.addEventListener("input", function(){
-    numDivs = document.getElementById('numSquareInput').value;
-    numSquares.textContent = numDivs;
-})
+/* Calculator functions */
+
+function getRandomColor(){
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
+}
